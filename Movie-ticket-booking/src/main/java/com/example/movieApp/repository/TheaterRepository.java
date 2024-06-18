3430c2fcedad22b27f888c12344248ca75daeff6
Sagar Shingare <124689778+sagarshingare16@users.noreply.github.com>
2024-06-19 00:03:55 +0530

    Delete Movie-ticket-booking/src directory

--- Movie-ticket-booking/src/main/java/com/example/movieApp/repository/TheaterRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.movieApp.repository;
-
-import com.example.movieApp.model.MovieTheater;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TheaterRepository extends JpaRepository<MovieTheater,Integer> {
-}
